39a2f0d6bb0a5fd370017cfca53f07a181d0851c
Daniel Gonzalez Briseno <sonusbeat@hotmail.com>
2020-05-02 18:58:41 -0500

    Image Validation Fixed

--- app/Http/Requests/StudioGalleryRequest.php
+++ app/Http/Requests/StudioGalleryRequest.php
@@ -27,8 +27,8 @@ class StudioGalleryRequest extends FormRequest
             'title' => 'required|between:4, 150',
             'slug' => 'required|between:4, 150',
             'description' => 'required|between:8, 200',
-            'image' => 'required|mimes:jpeg, jpg, png, gif|max:10000',
-            'image_alt' => 'required|between:8, 100',
+            'image' => 'nullable|mimes:jpeg, jpg, png, gif|max:10000',
+            'image_alt' => 'nullable|between:8, 100',
             'active' => 'nullable|boolean',
         ];
     }
@@ -50,7 +50,6 @@ class StudioGalleryRequest extends FormRequest
             'description.required' => 'La descripción de la imagen es obligatoria',
             'description.between' => 'El descripción de la imagen debe ser de :min a :max caracteres',
 
-            'image.required' => 'La descripción de la imagen es obligatoria',
             'image.mimes' => 'Solo se permite imagenes en formato jpeg, jpg, png, gif',
             'image.max' => 'Solo se permite subir imagenes inferiores a 10 Megabytes',
 
