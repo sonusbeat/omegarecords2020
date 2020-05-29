cff97d73e2a748720c18ca8b4bdf6b66d019a7a0
Daniel Gonzalez Briseno <sonusbeat@hotmail.com>
2020-05-28 20:35:07 -0500

    Google Analytics Added to public page

--- resources/views/templates/public/main.blade.php
+++ resources/views/templates/public/main.blade.php
@@ -30,6 +30,20 @@
         <link rel="stylesheet" href="{{ asset('css/ie.css') }}">
         <script src="{{ asset('js/html5shiv.js') }}"></script>
 		<![endif]-->
+
+      @if(env('analytics'))
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-167953316-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+
+          function gtag() { dataLayer.push(arguments); }
+
+          gtag('js', new Date());
+
+          gtag('config', 'UA-167953316-1');
+        </script>
+      @endif
 	</head>
 	<body>
 
