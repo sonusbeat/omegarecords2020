07be6c3aa3dc988b10cc2f23e795cf0b5ae268ac
Daniel Gonzalez Briseno <sonusbeat@hotmail.com>
2020-05-30 01:51:22 -0500

    WhatsApp Updated

--- resources/views/front/partials/facebook-messenger.blade.php
+++ resources/views/front/partials/facebook-messenger.blade.php
@@ -1,6 +1,28 @@
-<div class="whatsapp">
-    <a href="https://api.whatsapp.com/send?phone=5213314982299&text=&nbsp;Te&nbsp;podemos&nbsp;ayudar&nbsp;&iexcl;"
-       target="_blank">
-        <i class="fab fa-whatsapp text-white"></i>
-    </a>
+<!-- FACEBOOK MESSENGER -->
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function () {
+        FB.init({
+            xfbml: true,
+            version: 'v7.0'
+        });
+    };
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+     attribution=setup_tool
+     page_id="111657292252975"
+     theme_color="#f08a2a"
+     logged_in_greeting="¿Hola en que te podemos ayudar?"
+     logged_out_greeting="¿Hola en que te podemos ayudar?">
 </div>
