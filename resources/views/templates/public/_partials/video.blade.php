648760bdf7b58223f15ee8ccc222950246606126
Daniel Gonzalez Briseno <sonusbeat@hotmail.com>
2020-06-15 13:37:20 -0500

    Youtube video mute off

--- resources/views/templates/public/_partials/video.blade.php
+++ resources/views/templates/public/_partials/video.blade.php
@@ -1,5 +1,5 @@
 <div id="video" class="container">
     <div class="video-responsive">
-    <iframe src="https://www.youtube.com/embed/UVYfHN7wAwE?controls=0&loop=1&mute=1&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/UVYfHN7wAwE?controls=0&loop=1&mute=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </div>
